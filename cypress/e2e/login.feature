20c5848be62187ecd230fa20a55eac96b1a42c94
Anais Molina <anaismolinaantinao@gmail.com>
2026-04-07 22:39:15 -0400

    update login test

--- cypress/e2e/login.feature
+++ cypress/e2e/login.feature
@@ -4,8 +4,3 @@ Feature: User Login
     Given the user is on the login page
     When the user enters valid credentials
     Then the user should be logged in
-
-  Scenario: User logs in with invalid credentials
-    Given the user is on the login page
-    When the user enters invalid credentials
-    Then an error message should be displayed
--- cypress/e2e/step_definitions/login.js
+++ cypress/e2e/step_definitions/login.js
@@ -16,15 +16,3 @@ Then("the user should be logged in", function () {
   cy.url().should("include", "route=account/account");
   cy.contains("h2", "My Account").should("be.visible");
 });
-
-When("the user enters invalid credentials", function () {
-  cy.get("#input-email").type("invalid-user@example.com");
-  cy.get("#input-password").type("Invalid123", { log: false });
-  cy.get("input[type='submit'][value='Login']").click();
-});
-
-Then("an error message should be displayed", function () {
-  cy.get(".alert-danger")
-    .should("be.visible")
-    .and("contain", "Warning: No match for E-Mail Address and/or Password.");
-});
